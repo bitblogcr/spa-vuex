c435f0aada40bd18e49faed09ef684631225c50e
bitblogcr <161226820+bitblogcr@users.noreply.github.com>
2024-03-03 19:52:08 -0600

    Create TodoList.vue

---
+++ TodoList.vue
@@ -0,0 +1,26 @@
+<template> //plantilla de listado, recorrido con un ciclo
+  <div>
+    <h2>Lista de Pendientes</h2>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.description }} - {{ todo.date }}
+        <button @click="deleteTodo(todo.id)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    todos() {
+      return this.$store.state.todos;
+    },
+  },
+  methods: {
+    deleteTodo(id) {
+      this.$store.dispatch('deleteTodoAsync', id);
+    },
+  },
+};
+</script>
